d92c845d6202ef7b704f422bd06fb67eaf3c9018
Cybernetricks <bm21aac@herts.ac.uk>
2026-06-15 22:14:24 +0100

    Implemented_quotecontroller

---
+++ src/main/java/CompareTheMarket/example/demo/controller/QuoteController.java
@@ -0,0 +1,28 @@
+package CompareTheMarket.example.demo.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import CompareTheMarket.example.demo.model.QuoteRequest;
+import CompareTheMarket.example.demo.model.Quote;
+import CompareTheMarket.example.demo.service.QuoteService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/quotes")
+public class QuoteController {
+    
+    private final QuoteService quoteService;
+
+    public QuoteController(QuoteService quoteService) {
+        this.quoteService = quoteService;
+    }
+
+    @PostMapping
+    public List<Quote> getQuotes(@RequestBody QuoteRequest request) {
+        return quoteService.generateQuotes(request);
+    }
+}
