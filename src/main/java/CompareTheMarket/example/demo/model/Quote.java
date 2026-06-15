2f489bee62ca64b395056261ac42a3800b38969b
Cybernetricks <bm21aac@herts.ac.uk>
2026-06-15 19:16:47 +0100

    Added_constructors_and_getters

--- src/main/java/CompareTheMarket/example/demo/model/Quote.java
+++ src/main/java/CompareTheMarket/example/demo/model/Quote.java
@@ -6,4 +6,22 @@ public class Quote {
     private double price;
 
     private int excess;
+
+    public Quote(String provider, double price, int excess) {
+        this.provider = provider;
+        this.price = price;
+        this.excess = excess;
+    }
+
+    public String getProvider() {
+        return this.provider;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getExcess() {
+        return this.excess;
+    }
 }
--- src/main/java/CompareTheMarket/example/demo/model/QuoteRequest.java
+++ src/main/java/CompareTheMarket/example/demo/model/QuoteRequest.java
@@ -8,4 +8,27 @@ public class QuoteRequest {
     private int yearsDriving;
 
     private int claims;
+
+    public QuoteRequest(int age, int vehicleValue, int yearsDriving, int claims) {
+        this.age = age;
+        this.vehicleValue = vehicleValue;
+        this.yearsDriving = yearsDriving;
+        this.claims = claims;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public int getVehicleValue() {
+        return this.vehicleValue;
+    }
+
+    public int getYearsDriving() {
+        return this.yearsDriving;
+    }
+
+    public int getClaims() {
+        return this.claims;
+    }
 }
